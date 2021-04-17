4ebf9a169c15974532a39976bb8aaef41817405f
Karn Tiwari <53399509+Karn3003@users.noreply.github.com>
2021-04-17 12:14:37 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sign In | Pluralsight</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div id="wrapper">
+        <div id="left">
+            <div id="signin">
+                <div class="logo">
+                    <img src="./images/logo.png" alt="Pluralsight Clone">
+                </div>
+                <form action="">
+                    <div>
+                        <label for="">Email or Username</label>
+                        <input type="email" class="text-input" placeholder="">
+                    </div>
+                    <div>
+                        <label for="">Password</label>
+                        <input type="password" class="text-input" placeholder="">
+                    </div>
+                    <button type="submit" class="primary-btn">Sign In</button>
+                </form>
+                <div class="links">
+                    <a href="#">Forgot Password?</a>
+                    <a href="$">Sign in with company or school</a>
+                </div>
+                <hr class="bar">
+                <a href="#" class="secondary-btn">Create an account</a>
+            </div>
+            <footer id="main-footer">
+                <p>Copyright &copy; 2020, Pluralsight All Rights Reserved</p>
+                <div>
+                <a href="#">Terms of use</a> | <a href="#">Privacy Policy</a>
+                </div>
+            </footer>
+        </div>
+        <div id="right">
+            <div id="showcase">
+                <div id="header-image">
+                  <img src="images/skill-up.png" alt="Skill Up">
+                </div>
+                <div id="showcase-image">
+                  <img src="images/two-devs.png" alt="Two Devs">
+                  <img src="images/one-dev.png" alt="One Dev">
+                </div>
+              </div>
+        </div>
+    </div>
+</body>
+</html>
